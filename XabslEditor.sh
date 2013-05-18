1a9684e695d03f3a8e90dae85bd70cf9b9f21117
GiorgosMethe <giorgosmethe@gmail.com>
2013-05-18 18:09:30 +0200

    added script to download xabsleditor for compiling behaviors

---
+++ XabslEditor.sh
@@ -0,0 +1,3 @@
+wget https://launchpad.net/xabsleditor/1.x/1.0-rc2/+download/XabslEditor_1.0-rc2_source.zip
+unzip XabslEditor_1.0-rc2_source.zip -d NaoTH2011-light
+rm XabslEditor_1.0-rc2_source.zip
