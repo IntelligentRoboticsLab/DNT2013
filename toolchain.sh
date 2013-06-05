1db1a40876e7d5d244c32b291d85b18a32293df4
GiorgosMethe <giorgosmethe@gmail.com>
2013-06-06 00:52:08 +0200

    new toolchain script added

--- toolchain.sh
+++ toolchain.sh
@@ -1,7 +1,7 @@
-wget http://www2.informatik.hu-berlin.de/~naoth/ressources/toolchains/NaoTHToolChain_Ubuntu64.tar.xz
+wget http://www2.informatik.hu-berlin.de/~naoth/ressources/toolchains/NaoTHToolChain_Ubuntu64_453.tar.xz
 cd NaoTH2011-light
-tar -xvf ../NaoTHToolChain_Ubuntu64.tar.xz
-rm ../NaoTHToolChain_Ubuntu64.tar.xz
+tar -xvf ../NaoTHToolChain_Ubuntu64_453.tar.xz
+rm ../NaoTHToolChain_Ubuntu64_453.tar.xz
 cd NaoTHToolChain_Ubuntu64/toolchain_native/extern
 mkdir lib
 cd ../../
