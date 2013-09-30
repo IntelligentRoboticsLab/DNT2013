bae9dfb8bd2feae950cb16661d17c35582cb7c3b
GiorgosMethe <giorgosmethe@gmail.com>
2013-09-30 11:49:39 +0200

    added some packages to install in toolchain script

--- toolchain.sh
+++ toolchain.sh
@@ -1,3 +1,4 @@
+sudo apt-get install cmake build-essential zlib1g-dev gettext
 wget http://www2.informatik.hu-berlin.de/~naoth/ressources/toolchains/NaoTHToolChain_Ubuntu64_447.tar.xz
 cd NaoTH2011-light
 tar -xvf ../NaoTHToolChain_Ubuntu64_447.tar.xz
