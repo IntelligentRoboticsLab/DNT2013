ac8ed7009ee571969bb7088d7c0b4562afe23472
GiorgosMethe <giorgosmethe@gmail.com>
2013-12-11 14:07:45 +0100

    script for naoqisdk installation fixed

--- naoqisdk.sh
+++ naoqisdk.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/s/mvsmi9s15oxd264/naoqi-sdk-1.14.1-linux32.tar.gz
+wget https://www.dropbox.com/s/bb55zprtmcz7lpq/naoqi-sdk-1.14.5-linux32.tar.gz
 cd NaoTH2011-light
-tar -xvf ../naoqi-sdk-1.14.1-linux32.tar.gz
-rm ../naoqi-sdk-1.14.1-linux32.tar.gz
+tar -xvf ../naoqi-sdk-1.14.5-linux32.tar.gz
+rm ../naoqi-sdk-1.14.5-linux32.tar.gz
